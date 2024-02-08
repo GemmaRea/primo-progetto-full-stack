ca8a097606d4b28095b7e13b8303f6fcd7b82ffd
GemmaRea <130979811+GemmaRea@users.noreply.github.com>
2024-02-08 13:02:35 +0100

    create model and service

---
+++ front-end/e-commerce/src/app/componenti/paziente/paziente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paziente',
+  standalone: true,
+  imports: [],
+  templateUrl: './paziente.component.html',
+  styleUrl: './paziente.component.css'
+})
+export class PazienteComponent {
+
+}
